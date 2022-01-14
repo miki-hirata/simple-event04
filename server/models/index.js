a75eaa7d37991e4b2aede5e3ab04eec710881715
tangra <tangra.sd@gmail.com>
2022-01-14 10:39:21 +0900

    first commit

--- server/models/index.js
+++ server/models/index.js
@@ -4,16 +4,23 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+//const env = process.env.NODE_ENV || 'development';
+//const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
+/* let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+ */
+const url =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:postgres@localhost:5432/simple_event04";
+let sequelize = new Sequelize(url);
+
+
 
 fs
   .readdirSync(__dirname)
@@ -32,6 +39,5 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
